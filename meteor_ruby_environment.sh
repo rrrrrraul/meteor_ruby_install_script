7268215c61d8cd595d76794eb36e6aa0c0cfbefb
RM <rrrrrraul@users.noreply.github.com>
2017-08-19 16:40:26 +0100

    create first cut of file

---
+++ meteor_ruby_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+apt-get update  # To get the latest package lists
+
+#apt-get install <package name> -y
+
+
+#add repository that contains older version of NodeJS
+add-apt-repository ppa:chris-lea/node.js
+
+#install specific version of NodeJS
+apt-get install nodejs=0.10.37-1chl1~trusty1 -y
+
+#install MeteorJS vesion 1.0
+curl "https://install.meteor.com/?release=1.0" | sh
